7c34d5727b1b1384b73761f01879272966b84576
gavrinol <102408582+gavrinol@users.noreply.github.com>
2022-10-27 11:03:46 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/kurshogwarts/KurshogwartsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.kurshogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurshogwartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurshogwartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/kurshogwarts/KurshogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.kurshogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurshogwartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
